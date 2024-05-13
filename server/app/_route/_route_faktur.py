3ee6f6d77d240602898dae98428dd72474483993
Jeremy Edbert Widjaja <jeremy@cahayajakarta.com>
2024-05-13 17:19:51 +0700

    progress get faktur by id

--- server/app/_route/_route_faktur.py
+++ server/app/_route/_route_faktur.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, request
 from .._mods._mods_faktur import Faktur
 import json
 
@@ -9,24 +9,13 @@ faktur = Blueprint('faktur', __name__)
 def home():
     return render_template('home.html')
 
-# @faktur.route('/get-all')
-# def get_all_faktur_template():
-#     data = get_all_faktur().response
-#     return render_template('faktur_pajak.html', data=json.loads(data[0].decode('utf-8'))['data'])
-
-# @faktur.route('/get-id')
-# def get_faktur_by_id_template():
-#     data = get_all_faktur().response
-#     return render_template('faktur_pajak.html', data=json.loads(data[0].decode('utf-8'))['data'])
-
 @faktur.route('/faktur', methods=['GET'])
 def get_all_faktur():
-    return Faktur.get_all_faktur()
-
-
-@faktur.route('/faktur/id', methods=['GET'])
-def get_faktur_by_id():
-    return Faktur.get_faktur_by_id()
+    if not request.args.get('id'):
+        return Faktur.get_all_faktur()
+    else:
+        id = request.args.get('id')
+        return Faktur.get_faktur_by_id(id)
 
 
 @faktur.route('/faktur/date', methods=['GET'])

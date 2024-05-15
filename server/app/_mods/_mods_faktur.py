1e18d2aeb42d71c2cac26f2193ff67de0ef1a53a
Jeremy Edbert Widjaja <jeremy@cahayajakarta.com>
2024-05-15 15:47:51 +0700

    change the connection to cerm db

--- server/app/_mods/_mods_faktur.py
+++ server/app/_mods/_mods_faktur.py
@@ -5,10 +5,11 @@ from app.db_conn import connect_db, connect_db_server
 class Faktur:
     @staticmethod
     def get_all_faktur():
-        conn = connect_db()
-        cur = conn.cursor()
+        conn = connect_db_server()
+        cur = conn.cursor(as_dict=True)
 
-        query = "SELECT dossier_, dgbk_ref, fak__ref, bkj__ref, peri_ref, kla__ref, cde___ap FROM hafgfk__ WHERE cde___ap != ''"
+        query = "SELECT dossier_, dgbk_ref, fak__ref, bkj__ref, peri_ref, kla__ref, cde___ap FROM hafgfk__ WHERE cde___ap != '' ORDER BY bkj__ref DESC"
+        # query = "SELECT dossier_, dgbk_ref, fak__ref, bkj__ref, peri_ref, kla__ref, cde___ap FROM hafgfk__ WHERE cde___ap != '' LIMIT 10"
         cur.execute(query)
         result = cur.fetchall()
         if result is None:
@@ -24,8 +25,8 @@ class Faktur:
         if id is None:
             return make_response(jsonify({"message": "No data inside request body"}), 400)
 
-        conn = connect_db()
-        cur = conn.cursor()
+        conn = connect_db_server()
+        cur = conn.cursor(as_dict=True)
 
         query = "SELECT dossier_, dgbk_ref, fak__ref, bkj__ref, peri_ref, kla__ref, cde___ap FROM hafgfk__ WHERE fak__ref=%s AND cde___ap != ''"
         cur.execute(query, (id))
@@ -43,10 +44,10 @@ class Faktur:
         if start_date is None or end_date is None:
             return make_response(jsonify({"message": "No date"}), 400)
         
-        conn = connect_db()
-        cur = conn.cursor()
+        conn = connect_db_server()
+        cur = conn.cursor(as_dict=True)
 
-        query = "SELECT dossier_, dgbk_ref, fak__ref, bkj__ref, peri_ref, kla__ref, cde___ap, dok__dat FROM hafgfk__ WHERE dok__dat BETWEEN %s AND %s AND cde___ap != ''"
+        query = "SELECT dossier_, dgbk_ref, fak__ref, bkj__ref, peri_ref, kla__ref, cde___ap FROM hafgfk__ WHERE dok__dat BETWEEN %s AND %s AND cde___ap != ''"
         cur.execute(query, (start_date, end_date))
         result = cur.fetchall()
         cur.close()
@@ -68,7 +69,7 @@ class Faktur:
         id = data['id']
         year = data['year']
 
-        conn = connect_db()
+        conn = connect_db_server()
         cur = conn.cursor()
 
         query = "UPDATE hafgfk__ SET cde___ap = '' WHERE fak__ref = %s AND bkj__ref=%s"
@@ -80,8 +81,8 @@ class Faktur:
         result = cur.rowcount
         if result == 1:
             msg = "Value {} is removed".format(id)
-            log = "INSERT INTO log_del_fp (user_id, no_inv, no_fps, alasan, tgl_remove, jam_remove) VALUES (1, 2, 3, 'Hapus faktur', CURDATE(), CURTIME())"
-            cur.execute(log)
+            # log = "INSERT INTO log_del_fp (user_id, no_inv, no_fps, alasan, tgl_remove, jam_remove) VALUES (1, 2, 3, 'Hapus faktur', CURDATE(), CURTIME())"
+            # cur.execute(log)
         else:
             msg = "There is no value"
         response, status = {"message": msg}, 200

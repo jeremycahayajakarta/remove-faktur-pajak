9f0930d7ed0bfe7df661a8c8356212946d08f0ed
Jeremy Edbert Widjaja <jeremy@cahayajakarta.com>
2024-05-16 09:55:01 +0700

    add simple navbar -log

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Faktur from "../pages/Faktur";
+import Layout from "../layouts/Layout";
 
 const AppRoutes = () => {
   return (
     <Router>
-      <Routes>
-        <Route exact path={`/`} Component={Home} />
-        <Route path={`/faktur`} Component={Faktur} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route exact path={`/`} Component={Home} />
+          <Route path={`/faktur`} Component={Faktur} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
